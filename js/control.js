9989eb81f864bfad055ac30f631ba2f90a228930
evvolution <zhouxiaoemail@163.com>
2019-06-27 15:57:23 +0800

    upload

--- js/control.js
+++ js/control.js
@@ -267,10 +267,13 @@ function checkTimes(){
 		success:function(data){
 			var times = data.results.length;
 			if(times < 2){
+				alert("can vote")
 				return "can vote";
 			}else if((times > 2) || (times === 2)){
+				alert("cannot vote")
 				return "cannot vote";
 			}else {
+				alert("123")
 				return "123";
 			}
 		},
